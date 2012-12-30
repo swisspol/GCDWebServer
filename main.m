d483eb92cfbf021ea6ca34881012e5c7c31b7cc6
Pierre-Olivier Latour <pol@mac.com>
2012-12-29 22:44:33 -0800

    Added more test cases

--- main.m
+++ main.m
@@ -31,7 +31,55 @@ int main(int argc, const char* argv[]) {
   BOOL success = NO;
   @autoreleasepool {
     GCDWebServer* webServer = [[GCDWebServer alloc] init];
-    [webServer addHandlerForBasePath:@"/" localPath:NSHomeDirectory() indexFilename:nil cacheAge:0];
+    switch (0) {
+      
+      case 0: {
+        [webServer addHandlerForBasePath:@"/" localPath:NSHomeDirectory() indexFilename:nil cacheAge:0];
+        break;
+      }
+      
+      case 1: {
+        [webServer addDefaultHandlerForMethod:@"GET"
+                                 requestClass:[GCDWebServerRequest class]
+                                 processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
+          
+          return [GCDWebServerDataResponse responseWithHTML:@"<html><body><p>Hello World</p></body></html>"];
+          
+        }];
+        break;
+      }
+      
+      case 2: {
+        [webServer addHandlerForMethod:@"GET"
+                                  path:@"/"
+                          requestClass:[GCDWebServerRequest class]
+                          processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
+          
+          NSString* html = @" \
+            <html><body> \
+              <form name=\"input\" action=\"/\" method=\"post\" enctype=\"application/x-www-form-urlencoded\"> \
+              Value: <input type=\"text\" name=\"value\"> \
+              <input type=\"submit\" value=\"Submit\"> \
+              </form> \
+            </body></html> \
+          ";
+          return [GCDWebServerDataResponse responseWithHTML:html];
+          
+        }];
+        [webServer addHandlerForMethod:@"POST"
+                                  path:@"/"
+                          requestClass:[GCDWebServerURLEncodedFormRequest class]
+                          processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
+          
+          NSString* value = [[(GCDWebServerURLEncodedFormRequest*)request arguments] objectForKey:@"value"];
+          NSString* html = [NSString stringWithFormat:@"<html><body><p>%@</p></body></html>", value];
+          return [GCDWebServerDataResponse responseWithHTML:html];
+          
+        }];
+        break;
+      }
+      
+    }
     success = [webServer runWithPort:8080];
     [webServer release];
   }
