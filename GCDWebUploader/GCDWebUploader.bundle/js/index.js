73ff754de4d24956b8733f260c902c8bd229b0e9
Pierre-Olivier Latour <pol@mac.com>
2014-04-01 13:19:03 -0700

    Removed global progress bar

--- GCDWebUploader/GCDWebUploader.bundle/js/index.js
+++ GCDWebUploader/GCDWebUploader.bundle/js/index.js
@@ -146,13 +146,7 @@ $(document).ready(function() {
     dataType: 'json',
     
     start: function(e) {
-      $("#progress-bar").css("width", "0%");
       $(".uploading").show();
-    },
-    
-    progressall: function(e, data) {
-      var progress = parseInt(data.loaded / data.total * 100, 10);
-      $("#progress-bar").css("width", progress + "%");  // .text(progress + "%")
     },
     
     stop: function(e) {
