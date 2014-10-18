15caa9cd20ed6b32bd69704a90261efe1ca71b77
Pierre-Olivier Latour <pol@mac.com>
2014-10-18 12:42:38 -0700

    Fix

--- GCDWebServer/Core/GCDWebServerPrivate.h
+++ GCDWebServer/Core/GCDWebServerPrivate.h
@@ -57,6 +57,9 @@
 
 #define __GCDWEBSERVER_LOGGING_FACILITY_XLFACILITY__
 
+#undef XLOG_TAG
+#define XLOG_TAG @"gcdwebserver.internal"
+
 #import "XLFacilityMacros.h"
 
 #define GWS_LOG_DEBUG(...) XLOG_DEBUG(__VA_ARGS__)
