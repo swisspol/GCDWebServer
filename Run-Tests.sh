4ee9c309110af32998c8c4ba2b305ab07a37d1db
Pierre-Olivier Latour <pol@mac.com>
2015-11-01 13:12:57 -0800

    Fixed build script

--- Run-Tests.sh
+++ Run-Tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -ex
 
 OSX_SDK="macosx"
-if [ -z "$TRAVIS" ]; then
-  IOS_SDK="iphoneos"
-else
-  IOS_SDK="iphonesimulator"
-fi
+IOS_SDK="iphonesimulator"
+TVOS_SDK="appletvsimulator"
+
 OSX_SDK_VERSION=`xcodebuild -version -sdk | grep -A 1 '^MacOSX' | tail -n 1 |  awk '{ print $2 }'`
-IOS_SDK_VERSION=`xcodebuild -version -sdk | grep -A 1 '^iPhone' | tail -n 1 |  awk '{ print $2 }'`
+IOS_SDK_VERSION=`xcodebuild -version -sdk | grep -A 1 '^iPhoneOS' | tail -n 1 |  awk '{ print $2 }'`
+TVOS_SDK_VERSION=`xcodebuild -version -sdk | grep -A 1 '^AppleTVOS' | tail -n 1 |  awk '{ print $2 }'`
 
 OSX_TARGET="GCDWebServer (Mac)"
 IOS_TARGET="GCDWebServer (iOS)"
+TVOS_TARGET="GCDWebServer (tvOS)"
 CONFIGURATION="Release"
 
 OSX_TEST_SCHEME="GCDWebServers (Mac)"
@@ -38,21 +38,9 @@ function runTests {
 rm -rf "$BUILD_DIR"
 xcodebuild test -scheme "$OSX_TEST_SCHEME" "SYMROOT=$BUILD_DIR"
 
-# Build for iOS for oldest supported deployment target (TODO: run tests on iOS)
-rm -rf "$BUILD_DIR"
-xcodebuild build -sdk "$IOS_SDK" -target "$IOS_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "IPHONEOS_DEPLOYMENT_TARGET=5.1.1" > /dev/null
-
-# Build for iOS for current deployment target (TODO: run tests on iOS)
-rm -rf "$BUILD_DIR"
-xcodebuild build -sdk "$IOS_SDK" -target "$IOS_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "IPHONEOS_DEPLOYMENT_TARGET=$IOS_SDK_VERSION" > /dev/null
-
 # Build for OS X for oldest supported deployment target
 rm -rf "$BUILD_DIR"
 xcodebuild build -sdk "$OSX_SDK" -target "$OSX_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "MACOSX_DEPLOYMENT_TARGET=10.7" > /dev/null
-
-# Build for OS X for current deployment target
-rm -rf "$BUILD_DIR"
-xcodebuild build -sdk "$OSX_SDK" -target "$OSX_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "MACOSX_DEPLOYMENT_TARGET=$OSX_SDK_VERSION" > /dev/null
 
 # Run tests
 runTests $PRODUCT "htmlForm" "Tests/HTMLForm"
@@ -63,6 +51,22 @@ runTests $PRODUCT "webDAV" "Tests/WebDAV-Cyberduck"
 runTests $PRODUCT "webDAV" "Tests/WebDAV-Finder"
 runTests $PRODUCT "webUploader" "Tests/WebUploader"
 runTests $PRODUCT "webServer" "Tests/WebServer-Sample-Movie" "Tests/Sample-Movie.mp4"
+
+# Build for OS X for current deployment target
+rm -rf "$BUILD_DIR"
+xcodebuild build -sdk "$OSX_SDK" -target "$OSX_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "MACOSX_DEPLOYMENT_TARGET=$OSX_SDK_VERSION" > /dev/null
+
+# Build for iOS for oldest supported deployment target
+rm -rf "$BUILD_DIR"
+xcodebuild build -sdk "$IOS_SDK" -target "$IOS_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "IPHONEOS_DEPLOYMENT_TARGET=6.0" > /dev/null
+
+# Build for iOS for current deployment target
+rm -rf "$BUILD_DIR"
+xcodebuild build -sdk "$IOS_SDK" -target "$IOS_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "IPHONEOS_DEPLOYMENT_TARGET=$IOS_SDK_VERSION" > /dev/null
+
+# Build for tvOS for current deployment target
+rm -rf "$BUILD_DIR"
+xcodebuild build -sdk "$TVOS_SDK" -target "$TVOS_TARGET" -configuration "$CONFIGURATION" "SYMROOT=$BUILD_DIR" "TVOS_DEPLOYMENT_TARGET=$TVOS_SDK_VERSION" > /dev/null
 
 # Done
 echo "\nAll tests completed successfully!"
