10a94e36fdcfa3d86a781b58bfbda1aab31d91e7
John Scott <john@jjrscott.com>
2016-02-06 12:18:49 +0000

    Removed guards around __kindof usage as the project is Xcode 7 only.

--- GCDWebServer/Core/GCDWebServer.h
+++ GCDWebServer/Core/GCDWebServer.h
@@ -52,11 +52,7 @@ typedef GCDWebServerRequest* (^GCDWebServerMatchBlock)(NSString* requestMethod, 
  *  recommended to return a GCDWebServerErrorResponse on error so more useful
  *  information can be returned to the client.
  */
-#if __has_feature(objc_kindof)
 typedef GCDWebServerResponse* (^GCDWebServerProcessBlock)(__kindof GCDWebServerRequest* request);
-#else
-typedef GCDWebServerResponse* (^GCDWebServerProcessBlock)(GCDWebServerRequest* request);
-#endif
 
 /**
  *  The GCDWebServerAsynchronousProcessBlock works like the GCDWebServerProcessBlock
@@ -69,11 +65,7 @@ typedef GCDWebServerResponse* (^GCDWebServerProcessBlock)(GCDWebServerRequest* r
  *  useful information can be returned to the client.
  */
 typedef void (^GCDWebServerCompletionBlock)(GCDWebServerResponse* response);
-#if __has_feature(objc_kindof)
 typedef void (^GCDWebServerAsyncProcessBlock)(__kindof GCDWebServerRequest* request, GCDWebServerCompletionBlock completionBlock);
-#else
-typedef void (^GCDWebServerAsyncProcessBlock)(GCDWebServerRequest* request, GCDWebServerCompletionBlock completionBlock);
-#endif
 
 /**
  *  The port used by the GCDWebServer (NSNumber / NSUInteger).
