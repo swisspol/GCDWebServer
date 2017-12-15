7db34fe98fb0613c25a63c0a733651b64b166782
Pavel Alexeev <pasha.alexeev@gmail.com>
2017-12-15 18:09:53 +0300

    Add check for browsers that doesn't support relativePath

--- GCDWebUploader/GCDWebUploader.bundle/Contents/Resources/js/index.js
+++ GCDWebUploader/GCDWebUploader.bundle/Contents/Resources/js/index.js
@@ -211,7 +211,7 @@ $(document).ready(function() {
     
     add: function(e, data) {
       var file = data.files[0];
-      var relativePath = _keepDirectoryTree ? file.relativePath : "";
+      var relativePath = (_keepDirectoryTree && file.relativePath !== undefined) ? file.relativePath : "";
       data.formData = {
         path: _path + relativePath
       };
