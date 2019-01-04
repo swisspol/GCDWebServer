b060305d6dd450733a212c687a94c6047d7b639f
Pierre-Olivier Latour <pol@mac.com>
2019-01-03 17:35:35 -0800

    Only fallback to -[NSData base64Encoding] on macOS prior to 10.9

--- GCDWebServer/Core/GCDWebServer.m
+++ GCDWebServer/Core/GCDWebServer.m
@@ -415,7 +415,7 @@ static inline id _GetOption(NSDictionary<NSString*, id>* options, NSString* key,
 
 static inline NSString* _EncodeBase64(NSString* string) {
   NSData* data = [string dataUsingEncoding:NSUTF8StringEncoding];
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE || (__MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_9)
   return [[NSString alloc] initWithData:[data base64EncodedDataWithOptions:0] encoding:NSASCIIStringEncoding];
 #else
   if (@available(macOS 10.9, *)) {
