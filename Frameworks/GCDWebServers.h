e561389d33b94875a42f0587483a7b3b04901914
Pierre-Olivier Latour <pol@mac.com>
2019-01-06 11:58:43 -0800

    Use explicit modulemap for frameworks

--- Frameworks/GCDWebServers.h
+++ Frameworks/GCDWebServers.h
@@ -26,27 +26,27 @@
  */
 
 // GCDWebServer Core
-#import <GCDWebServers/GCDWebServer.h>
-#import <GCDWebServers/GCDWebServerConnection.h>
-#import <GCDWebServers/GCDWebServerFunctions.h>
-#import <GCDWebServers/GCDWebServerHTTPStatusCodes.h>
-#import <GCDWebServers/GCDWebServerResponse.h>
-#import <GCDWebServers/GCDWebServerRequest.h>
+#import "GCDWebServer.h"
+#import "GCDWebServerConnection.h"
+#import "GCDWebServerFunctions.h"
+#import "GCDWebServerHTTPStatusCodes.h"
+#import "GCDWebServerResponse.h"
+#import "GCDWebServerRequest.h"
 
 // GCDWebServer Requests
-#import <GCDWebServers/GCDWebServerDataRequest.h>
-#import <GCDWebServers/GCDWebServerFileRequest.h>
-#import <GCDWebServers/GCDWebServerMultiPartFormRequest.h>
-#import <GCDWebServers/GCDWebServerURLEncodedFormRequest.h>
+#import "GCDWebServerDataRequest.h"
+#import "GCDWebServerFileRequest.h"
+#import "GCDWebServerMultiPartFormRequest.h"
+#import "GCDWebServerURLEncodedFormRequest.h"
 
 // GCDWebServer Responses
-#import <GCDWebServers/GCDWebServerDataResponse.h>
-#import <GCDWebServers/GCDWebServerErrorResponse.h>
-#import <GCDWebServers/GCDWebServerFileResponse.h>
-#import <GCDWebServers/GCDWebServerStreamedResponse.h>
+#import "GCDWebServerDataResponse.h"
+#import "GCDWebServerErrorResponse.h"
+#import "GCDWebServerFileResponse.h"
+#import "GCDWebServerStreamedResponse.h"
 
 // GCDWebUploader
-#import <GCDWebServers/GCDWebUploader.h>
+#import "GCDWebUploader.h"
 
 // GCDWebDAVServer
-#import <GCDWebServers/GCDWebDAVServer.h>
+#import "GCDWebDAVServer.h"
